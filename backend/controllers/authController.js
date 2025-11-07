81fb570cbfcc91a92f3fe681ebd3b4d2ae2034b5
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2025-11-07 15:01:10 +0100

    feat(auth): create auth controller layer

---
+++ backend/controllers/authController.js
@@ -0,0 +1,25 @@
+const authService = require('../services/authService');
+
+const handleRegister = async (req, res) => {
+    try {
+        const {username, email, password} = req.body;
+
+        if (!username || !email || !password)
+            return res.status(400).json({message: 'All fields are required.'});
+
+        const {user, token} = await authService.registerUser(username, email, password);
+
+        res.status(201).json({
+            message: 'Registration succesful.',
+            user: user,
+            token: token
+        });
+
+    } catch (error) {
+        res.status(400).json({message: error.message || 'Registration failed.'})
+    }
+};
+
+module.exports = {
+    handleRegister
+}
