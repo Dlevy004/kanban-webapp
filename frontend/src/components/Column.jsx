b8fb600367f9f7eee8ae542987582e5536593c2b
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2025-11-13 14:04:32 +0100

    feat(board): create Task component and render task in Column

--- frontend/src/components/Column.jsx
+++ frontend/src/components/Column.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Column.css';
+import Task from './Task.jsx';
 
 function Column({ columnData }) {
 
@@ -8,7 +9,9 @@ function Column({ columnData }) {
             <div className="column-title">{columnData.title}</div>
 
             <div className="tasks-list">
-
+                {columnData.tasks.map(task => (
+                    <Task key={task._id} taskData={task} />
+                ))}
             </div>
 
             <button className="new-task-button">
