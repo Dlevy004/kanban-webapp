4db61685f98602ad0db5673bd05dacb491cab62c
Czeitner András <czeitnerandras@mailbox.unideb.hu>
2025-11-15 23:50:35 +0100

    feat(theme): wrap App component with ThemeProvider for theme context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './global.css' 
+
 import App from './App.jsx'
-import '../src/global.css'
+import { ThemeProvider } from './theme/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
