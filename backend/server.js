0ee03761ff4b9da7bb557fa50ce13948504c621d
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2025-11-27 14:59:06 +0100

    fix(server.js): extends express request body size

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ const MONGO_URI = process.env.MONGO_URI;
 const app = express();
 
 app.use(cors()); 
-app.use(express.json()); 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
