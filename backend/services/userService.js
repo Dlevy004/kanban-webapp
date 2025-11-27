19f304dde6d52b3272874e91baf3366c7dbb9893
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2025-11-27 12:25:51 +0100

    feat: implement business logic in userService

---
+++ backend/services/userService.js
@@ -0,0 +1,26 @@
+const User = require('../models/userModel');
+
+const updateUserProfile = async (userId, updateData) => {
+    const allowedUpdates = {};
+
+    if (updateData.username) allowedUpdates.username = updateData.username;
+    
+    const updateUser = await User.findByIdAndUpdate(
+        userId,
+        allowedUpdates,
+        {
+            new: true,
+            runValidators: true
+        }
+    ).select('-passwordHash');
+
+    if (!updateUser) {
+        throw new Error('User not found');
+    }
+    
+    return updateUser;
+}
+
+module.exports = {
+    updateUserProfile
+}
