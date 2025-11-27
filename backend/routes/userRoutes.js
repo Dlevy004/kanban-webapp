a663abfe57fe0dae2b411b22733b84d85d1e9e98
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2025-11-27 13:29:15 +0100

    fix: correct middleware import

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const protect = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/authMiddleware');
 
 router.put('/profile', protect, userController.handleUpdateUserProfile);
 
